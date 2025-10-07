a1757d29575d35d168b98530346b0c83430be3e7
Erick Borges dos Santos <135672557+ErickBorgess@users.noreply.github.com>
2025-10-07 09:12:49 -0300

    Inicio do projeto

---
+++ src/main/java/br/com/jairinho/jmc_estoque/JmcEstoqueApplication.java
@@ -0,0 +1,13 @@
+package br.com.jairinho.jmc_estoque;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JmcEstoqueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JmcEstoqueApplication.class, args);
+	}
+
+}

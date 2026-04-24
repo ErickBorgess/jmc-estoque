23136b5ed5e420ad0b053b407caf768b0ecbaad5
Erick Borges dos Santos <erickborges04@hotmail.com>
2026-04-24 17:05:59 -0300

    Upload e Fuso Horario
    Modificações na lógica de upload e implementação de fuso horário de Brasília.

--- src/main/java/br/com/jairinho/jmc_estoque/JmcEstoqueApplication.java
+++ src/main/java/br/com/jairinho/jmc_estoque/JmcEstoqueApplication.java
@@ -1,26 +1,26 @@
 package br.com.jairinho.jmc_estoque;
 
-import org.springframework.boot.CommandLineRunner;
+//import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import br.com.jairinho.jmc_estoque.repository.ProdutoRepository;
-import br.com.jairinho.jmc_estoque.service.DadosCSV;
+//import br.com.jairinho.jmc_estoque.repository.ProdutoRepository;
+//import br.com.jairinho.jmc_estoque.service.DadosCSV;
 
 @SpringBootApplication
-public class JmcEstoqueApplication implements CommandLineRunner {
-	private final DadosCSV dadosCSV;
+public class JmcEstoqueApplication {
+	/*private final DadosCSV dadosCSV;
 	private final ProdutoRepository produtoRepository;
 
 	public JmcEstoqueApplication(DadosCSV dadosCSV, ProdutoRepository produtoRepository) {
         this.dadosCSV = dadosCSV;
         this.produtoRepository = produtoRepository;
-    }
+    }*/
 	
 	public static void main(String[] args) {
 		SpringApplication.run(JmcEstoqueApplication.class, args);
 	}
 
-	@Override
+	/*@Override
 	public void run(String... args) throws Exception {
 		if (produtoRepository.count() == 0) {
             System.out.println("Banco de dados vazio. Carregando CSV base...");
@@ -28,6 +28,6 @@ public class JmcEstoqueApplication implements CommandLineRunner {
         } else {
             System.out.println("Banco de dados preenchido.");
         }
-	}
+	}*/
 
 }
--- src/main/java/br/com/jairinho/jmc_estoque/model/Produto.java
+++ src/main/java/br/com/jairinho/jmc_estoque/model/Produto.java
@@ -1,6 +1,6 @@
 package br.com.jairinho.jmc_estoque.model;
 
-import java.time.LocalDateTime;
+import java.time.ZonedDateTime;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -25,5 +25,5 @@ public class Produto {
     private String nome;
     private Double preco;
     private String marca;
-    private LocalDateTime dataAtualizacao;
+    private ZonedDateTime dataAtualizacao;
 }
